91c22773e8f0079f930994182f7471704ed7509e
Edi <130770149+BlusyLake@users.noreply.github.com>
2025-11-25 21:36:05 -0300

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+# --- Identificação ---
+variable "compartment_id" {
+  description = "OCID do compartimento onde tudo será criado"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Prefixo para nomear os recursos (Ex: Blusy-Prod, Blusy-Dev)"
+  type        = string
+  default     = "Blusy-Padrao"
+}
+
+# --- Acesso ---
+variable "ssh_public_key" {
+  description = "Chave pública para acessar as VMs"
+  type        = string
+}
+
+# --- Hardware (Shape) ---
+variable "instance_shape" {
+  description = "Qual hardware usar? (Padrão: Ampere ARM Grátis)"
+  default     = "VM.Standard.A1.Flex"
+}
+
+variable "instance_ocpus" {
+  default = 4
+}
+
+variable "instance_memory" {
+  default = 24
+}
